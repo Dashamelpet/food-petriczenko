3683e892fb84116d08463d1e37fbd92e2569a826
Daria <dashapetrash@gmail.com>
2024-11-08 13:43:56 +0100

    08.11.2024

--- src/js/main.js
+++ src/js/main.js
@@ -139,5 +139,56 @@ window.addEventListener('DOMContentLoaded', () => {
     }
     window.addEventListener('scroll', showModalScroll);
     
-});
 
+    // Create cards
+
+    
+
+    class Card {
+        constructor(src, title, descr, price, parentSelector, ...classes) {
+            this.parent = document.querySelector(parentSelector);
+            this.classes = classes;
+            this.src = src;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+
+        render() {
+            let element = document.createElement('div');
+            if(this.classes.length === 0) {
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            }else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+            
+            element.innerHTML = `
+                <img src="${this.src}" alt="elite">
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+                `
+            ;
+            this.parent.append(element);
+        }
+       
+    };
+    
+    new Card(
+        'img/tabs/vegy.jpg',
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд:',
+        9,
+        '.menu__field .container'
+        ).render();
+});
